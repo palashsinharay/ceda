cef4a3f30fd3e4fcb1311d7db7657dc51392a139
bunty <sahani.bunty9@gmail.com>
2013-09-03 11:26:47 +0530

    search result  limit removed

--- application/models/cms.php
+++ application/models/cms.php
@@ -391,24 +391,29 @@ class Cms extends CI_Model {
        function get_search_list($search_keyword)
 	{
                
-        $sql = "SELECT * FROM blog WHERE MATCH (`desc`) AGAINST (? IN BOOLEAN MODE )limit 2";  
+        //$sql = "SELECT * FROM blog WHERE MATCH (`desc`) AGAINST (? IN BOOLEAN MODE )limit 2";  
+        $sql = "SELECT * FROM blog WHERE MATCH (`desc`) AGAINST (? IN BOOLEAN MODE )";  
         $query = $this->db->query($sql, array($search_keyword) );
         $result['blog'] = $query->result();
         
-        $sql1 = "SELECT * FROM cmspage WHERE MATCH (`content`) AGAINST (? IN BOOLEAN MODE )limit 2";  
+       // $sql1 = "SELECT * FROM cmspage WHERE MATCH (`content`) AGAINST (? IN BOOLEAN MODE )limit 2";  
+         $sql1 = "SELECT * FROM cmspage WHERE MATCH (`content`) AGAINST (? IN BOOLEAN MODE )";  
         $query = $this->db->query($sql1, array($search_keyword) );
         $result['cms'] = $query->result();
         
-        $sql2 = "SELECT * FROM product WHERE MATCH (`desc`) AGAINST (? IN BOOLEAN MODE ) limit 2";  
+       // $sql2 = "SELECT * FROM product WHERE MATCH (`desc`) AGAINST (? IN BOOLEAN MODE ) limit 2";  
+         $sql2 = "SELECT * FROM product WHERE MATCH (`desc`) AGAINST (? IN BOOLEAN MODE )";  
         $query = $this->db->query($sql2, array($search_keyword) );
         $result['product'] = $query->result();
 
-        $sql3 = "SELECT * FROM service WHERE MATCH (`s_desc`) AGAINST (? IN BOOLEAN MODE )limit 2";  
+       // $sql3 = "SELECT * FROM service WHERE MATCH (`s_desc`) AGAINST (? IN BOOLEAN MODE )limit 2";  
+         $sql3 = "SELECT * FROM service WHERE MATCH (`s_desc`) AGAINST (? IN BOOLEAN MODE )";  
         $query = $this->db->query($sql3, array($search_keyword) );
         //echo $this->db->last_query();
         $result['service'] = $query->result();
 
-        $sql3 = "SELECT * FROM news WHERE MATCH (`description`) AGAINST (? IN BOOLEAN MODE ) limit 2";  
+       // $sql3 = "SELECT * FROM news WHERE MATCH (`description`) AGAINST (? IN BOOLEAN MODE ) limit 2";  
+         $sql3 = "SELECT * FROM news WHERE MATCH (`description`) AGAINST (? IN BOOLEAN MODE )";  
         $query = $this->db->query($sql3, array($search_keyword) );
         //echo $this->db->last_query();
         $result['news'] = $query->result();
